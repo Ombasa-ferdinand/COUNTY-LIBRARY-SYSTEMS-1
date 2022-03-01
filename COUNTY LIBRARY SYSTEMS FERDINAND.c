ac0912d6e74a4c761c0289dbd757d7362a1ea6ec
Ombasa-ferdinand <99478277+Ombasa-ferdinand@users.noreply.github.com>
2022-03-01 22:34:05 +0300

    Add files via upload

---
+++ COUNTY LIBRARY SYSTEMS FERDINAND.c
@@ -0,0 +1,22 @@
+/*
+Project: County Library management system.
+Author: Ombasa Ferdinand
+Date : Oct 2021
+Compiler: GNU GCC
+Language: C99
+License: MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("\tCounty Library\n");
+	printf("Welcome Mr. Ombasa. \n");
+	printf("what would you like to do?\n");
+	printf("1. Add new User\n");
+	printf("2. Add Book.\n");
+	printf("selected action: ");
+	return 0;
+}
+
